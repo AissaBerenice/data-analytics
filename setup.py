fcd0f910a6727b26c5be76266f0607b173a914ab
Aissabf <aissafosado@gmail.com>
2024-02-02 08:10:23 -0600

    Cookiecutter Installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project of lectures on Data Analytics Course',
+    author='ITESO',
+    license='MIT',
+)
